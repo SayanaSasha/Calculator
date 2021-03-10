15b2516ec93fdc86355c28fae4b054f423858096
Sayana13 <domshoevas@gmail.com>
2021-03-10 12:14:30 -0800

    components were added

--- src/Debt.js
+++ src/Debt.js
@@ -1,10 +1,46 @@
+import React from 'react';
 
 
-function Debt () {
-    return (
-        <div>
-            All loans, Credit cards and debts
-        </div>
-    )
+class Debt extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            income: [],
+            typeOfDebt: '',
+            amountOfDebt: '',
+            total: 0,
+            editing: false
+        };
+    }
+
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <p> Please enter here all loans, credit cards and debts</p>
+                    <input type="text"
+                           placeholder={'Type of debt'}
+                           value={this.state.typeOfDebt}
+                           onChange={(event) =>
+                               this.setState({typeOfDebt: event.target.value})
+                           }
+                    />
+
+                    <input
+                        type="number"
+                        placeholder="Amount"
+                        value={this.state.amountOfDebt}
+                        onChange={(event) =>
+                            this.setState({amountOfDebt: event.target.value})
+                        }
+                    />
+                    <button>Ok</button>
+                </form>
+            </div>
+        )
+    }
 }
 export default Debt;
+
+
--- src/Expenses.js
+++ src/Expenses.js
@@ -1,76 +1,75 @@
 import React from 'react';
 
 class Expenses extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      expenses: [],
-      typeOfExp: '',
-      amountOfExp: 0,
-      total: 0,
-    };
-  }
+    constructor() {
+        super();
+        this.state = {
+            expenses: [],
+            typeOfExp: '',
+            amountOfExp: '',
+            total: 0,
+        };
+    }
 
-  render() {
-    return (
-      <div className="Expenses">
-        <h1>Financial Calculator</h1>
-        <hr />
-        <h2>Enter your expenses here</h2>
-        <form>
-          <input
-            type="text"
-            placeholder="Name of Expenses"
-            value={this.state.typeOfExp}
-            onChange={(event) => this.setState({typeOfExp: event.target.value})}
-          />
+    render() {
+        return (
+            <div className="Expenses">
 
-          <input
-            type="number"
-            placeholder="Amount"
-            value={this.state.amountOfExp}
-            onChange={(event) =>
-              this.setState({amountOfExp: event.target.value})
-            }
-          />
+                <h2>Enter all types of your expenses here</h2>
+                <form>
+                    <input
+                        type="text"
+                        placeholder="Name of Expenses"
+                        value={this.state.typeOfExp}
+                        onChange={(event) => this.setState({typeOfExp: event.target.value})}
+                    />
 
-          <button
-            onClick={(event) => {
-              event.preventDefault();
-              this.setState({
-                income: [
-                  ...this.state.income,
-                  ...[[this.state.typeOfExp, this.state.amountOfExp]],
-                ],
-              });
-              this.setState({
-                total:
-                  Number(this.state.total) + Number(this.state.amountOfExp),
-              });
-              this.setState({typeOfExp: '', amountOfExp: 0});
-            }}
-          >
-            Ok
-          </button>
-        </form>
+                    <input
+                        type="number"
+                        placeholder="Amount"
+                        value={this.state.amountOfExp}
+                        onChange={(event) =>
+                            this.setState({amountOfExp: event.target.value})
+                        }
+                    />
 
-        <h3>All type of expenses</h3>
-        <li style={{listStyleType: 'none'}}>
-          {this.state.expenses.map((type, index) => {
-            return (
-              <ul key={index}>
-                <h4>
-                  {type[0]} - {type[1]}
-                </h4>
-              </ul>
-            );
-          })}
-        </li>
-        <hr />
-        <h4>Total: {this.state.total}</h4>
-      </div>
-    );
-  }
+                    <button
+                        onClick={(event) => {
+                            event.preventDefault();
+                            this.setState({
+                                expenses: [
+                                    ...this.state.expenses,
+                                    ...[[this.state.typeOfExp, this.state.amountOfExp]],
+                                ],
+                            });
+                            this.setState({
+                                total:
+                                    Number(this.state.total) + Number(this.state.amountOfExp),
+                            });
+                            this.setState({typeOfExp: '', amountOfExp: 0});
+                        }}
+                    >
+                        Ok
+                    </button>
+                </form>
+
+                <h3>All type of expenses</h3>
+                <li style={{listStyleType: 'none'}}>
+                    {this.state.expenses.map((type, index) => {
+                        return (
+                            <ul key={index}>
+                                <h4>
+                                    {type[0]} - {type[1]}
+                                </h4>
+                            </ul>
+                        );
+                    })}
+                </li>
+                <hr />
+                <h4>Total: {this.state.total}</h4>
+            </div>
+        );
+    }
 }
 
 export default Expenses;
