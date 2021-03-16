5961f699bd517817ee3f43380499ca693bd32f04
Sayana13 <domshoevas@gmail.com>
2021-03-15 22:47:33 -0700

    Updated component Expenses

--- src/Expenses.js
+++ src/Expenses.js
@@ -54,21 +54,74 @@ class Expenses extends React.Component {
                     </button>
                 </form>
 
-                <h3>All type of expenses</h3>
-                <li style={{listStyleType: 'none'}}>
-                    {this.state.expenses.map((type, index) => {
-                        return (
-                            <ul key={index}>
-                                <h4>
-                                    {type[0]} - {type[1]}
-                                </h4>
-                            </ul>
-                        );
-                    })}
-                </li>
-                <hr />
+                <h3>All your expenses</h3>
+                {this.state.editing ? (
+                    <li style={{listStyleType: 'none'}}>
+                        {this.state.expenses.map((type, index) => {
+                            return (
+                                <ul key={index}>
+                                    <input type="text"
+                                           defaultValue={type [0]}
+                                           onChange={(event) => {
+                                               const newExpenses = this.state.expenses.map((el, i) => {
+                                                   if (i === index) el = [event.target.value, type[1]];
+                                                   return el;
+                                               });
+                                               this.setState({expenses: newExpenses});
+                                           }}
+                                    />
+                                    <input type="number"
+                                           defaultValue={type[1]}
+                                           onChange={(event) => {
+                                               const newExpenses = this.state.expenses.map((el, i) => {
+                                                   if (i === index) el = [type[0], event.target.value];
+                                                   return el;
+                                               });
+                                               this.setState({expenses: newExpenses});
+                                           }}
+                                    />
+                                </ul>
+                            );
+                        })}
+                    </li>
+                ) : (
+                    <li style={{listStyleType: 'none'}}>
+                        {this.state.expenses.map((type, index) => {
+                            return (
+                                <ul key={index}>
+                                    <h4>{type[0]} -${type[1]}</h4>
+                                    <button onClick={() => this.setState({
+                                        expenses: this.state.expenses.filter(
+                                            (el, i) => i !== index
+                                        ),
+                                    })
+                                    }
+                                    >Remove
+                                    </button>
+                                </ul>
+                            );
+                        })}
+                    </li>
+                )}
+                {this.state.editing ? (
+                    <button onClick={() => {
+                        this.setState({editing: false});
+                        this.setState({
+                            total: this.state.expenses.reduce((sum, el) => {
+                                return sum + Number(el[1]);
+                            }, 0)
+                        });
+                    }}
+                    > Save</button>
+                ) : (
+                    <button onClick={() => this.setState({editing: true})}
+                    >Edit</button>
+                )}
+                <hr/>
                 <h4>Total: {this.state.total}</h4>
-                <Link to="/savings"><button>Next</button></Link>
+                <Link to="/savings">
+                    <button>Next</button>
+                </Link>
             </div>
         );
     }
